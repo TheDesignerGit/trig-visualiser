a355770e6016336e0887fcfd903249ca794caf9d
JayJay Abrahams <jayson_lastong@yahoo.co.za>
2024-12-18 14:00:47 +0200

    implement interactivity

--- app.js
+++ app.js
@@ -1,34 +1,91 @@
-const canvas = document.getElementById("myCanvas")
-const ctx = canvas.getContext("2d")
+const myCanvas = document.getElementById("myCanvas")
+const ctx = myCanvas.getContext("2d")
 
 const offset = {
-    x: canvas.width / 2,
-    y: canvas.height/ 2
+    x: myCanvas.width / 2,
+    y: myCanvas.height/ 2
 }
 
-ctx.translate(offset.x, offset.y)
+ctx.translate(offset.x, offset.y)               // make center the origin pont
 
 const A = { x: 0, y: 0 }
 const B = { x: 90, y: 120 }
 const C = { x: B.x, y: 0 }
 
-const drawPoint = (location, size=20, colour ='black') => {
+update()        // to avoid blank screen at first, run update
+document.onmousemove = (event) => {
+    // console.log('x: ', event.x, "y: ", event.y)
+    B.x = event.x - offset.x          // set point x-value to x-value of the event
+    B.y = event.y - offset.y
+    C.x = B.x
+
+    update()
+}
+
+function update () {
+    const c = distance(A,B)
+    const b = distance(A,C)
+    const a = distance(B,C)
+
+    ctx.clearRect(-offset.x, -offset.y, myCanvas.width, myCanvas.height)
+
+    drawCoordinateSystem(ctx, offset) 
+
+    drawLine(A,B)           // draw the connecting lines
+    drawText('c', average(A,B), "green")
+    drawLine(A,C)
+    drawText('b', average(A,C), "green")
+    drawLine(B,C)
+    drawText('a', average(B,C), "green")
+
+    // drawPoint(A)
+    // drawText('A', A)
+    // drawPoint(B)
+    // drawText('B', B)
+    // drawPoint(C)
+    // drawText('C', C)
+}
+
+function distance (p1, p2) {
+    return Math.hypot(p1.x - p2.x, p1.y - p2.y)
+    
+}
+function average (p1, p2) {
+    return {
+        x: (p1.x + p2.x) / 2,
+        y: (p1.y + p2.y) / 2
+    }
+}
+
+function drawPoint (location, size=20, colour ='black') {
     ctx.beginPath()
     ctx.fillStyle = colour
-    ctx.arc(location.x, location.y, size/2, 0, Math.PI*2)
+    ctx.arc(location.x, location.y, size/2, 0, Math.PI*2)       // ?draw small circle
     ctx.fill()
 }
 
-const drawText = (text, location, colour = 'white') => {
+function drawLine (point1, point2, colour = 'black') {
+    ctx.beginPath()
+    ctx.lineWidth = 2
+    ctx.strokeStyle = colour
+    ctx.moveTo(point1.x, point1.y)
+    ctx.lineTo(point2.x, point2.y)
+    ctx.stroke()
+}
+
+function drawText (text, location, colour = 'white') {
     ctx.beginPath()
     ctx.fillStyle = colour
     ctx.textAlign = 'center'
     ctx.textBaseline = 'middle'
-    ctx.font = 'bold 13px Courier'
+    ctx.font = 'bold 18px Courier'
+    ctx.strokeStyle='white'
+    ctx.lineWidth = 6
+    ctx.strokeText(text, location.x, location.y) 
     ctx.fillText(text, location.x, location.y) 
 }
 
-const drawCoordinateSystem = (ctx, offset) => {
+function drawCoordinateSystem (ctx, offset) {
     ctx.beginPath()
     ctx.moveTo(-offset.x, 0)
     ctx.lineTo(ctx.canvas.width - offset.x, 0)
@@ -41,12 +98,3 @@ const drawCoordinateSystem = (ctx, offset) => {
     
     ctx.setLineDash([])             // reset after it's drawn, to avoid affect anything drawn afterwards
 }
-
-drawCoordinateSystem(ctx, offset) 
-
-drawPoint(A)
-drawText('A', A)
-drawPoint(B)
-drawText('B', B)
-drawPoint(C)
-drawText('C', C)
